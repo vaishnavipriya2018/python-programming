183bea88447b419db94299e2875a311715701368
vaishnavipriya2018 <vaishalipriya152@gmail.com>
2025-09-10 17:36:10 +0530

    Add files via upload

---
+++ list operation.py
@@ -0,0 +1,16 @@
+a=[]
+n=int(input("Enter the no of elements:"))
+for i in range (0,n):
+    m=int(input("enter the element:"))
+    a.append(m)
+print(a)
+print(a[0])
+print(a[5])
+print(a[2])
+print(a[3:])
+print(a[3:])
+a.extend(7,8,9)
+print(a)
+
+
+
